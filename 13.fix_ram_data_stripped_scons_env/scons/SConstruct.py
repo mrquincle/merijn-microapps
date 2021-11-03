893a5d8b4c2f34a04f4bc753ef4ea848f2a4b34e
Anne van Rossum <anne@crownstone.rocks>
2021-11-03 13:38:11 +0100

    Same checksum (first step, namely 52016) as in crownstone-microapp example

--- 13.fix_ram_data_stripped_scons_env/scons/SConstruct.py
+++ 13.fix_ram_data_stripped_scons_env/scons/SConstruct.py
@@ -13,7 +13,8 @@ def create_empty_header_symbols_file(target, source, env):
     for l in lines:
         print(l, end='')
 
-env = DefaultEnvironment()
+#env = DefaultEnvironment()
+env = Environment(ENV = {'PATH' : os.environ['PATH']})
 
 env.Append(
     CWD = './'
@@ -31,7 +32,7 @@ env.Append(
     HEADER_LD_EMPTY =  join(env['LINKER_DIR'], "microapp_header_symbols_empty.ld"),
     SYMBOLS_LD = join(env['LINKER_DIR'], "microapp_symbols.ld"),
     SYMBOLS_IN = join(env['LINKER_DIR'], "microapp_symbols.ld.in"),
-    MAIN_C = join(env['SRC'], "main.c"),
+    MAIN_C = join(env['SRC'], "example.c"),
     MAIN_ELF =  join(env['BUILD'], "main.elf"),
     MAIN_ELF_TMP = join(env['BUILD'], "main.elf.tmp"),
     MAIN_BIN_TMP = join(env['BUILD'], "main.bin.tmp"),
@@ -45,19 +46,19 @@ env.Append(
 	  -nostdlib -ffreestanding -fno-threadsafe-statics
 	  -Wl,--gc-sections
 	  -Wno-error=unused-function -Os -fomit-frame-pointer -Wl,-z,nocopyreloc
-	  -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -u _printf_float""".split()
+	  -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -u _printf_float""".split(),
 )
 
-core_files="Arduino.c main.c microapp.c Serial.cpp Wire.cpp ipc/cs_IpcRamData.c".split()
+core_files="main.c microapp.c Arduino.c Wire.cpp Serial.cpp ipc/cs_IpcRamData.c".split()
 for index, file_ in enumerate(core_files):
     core_files[index] = join(env['CORE'], file_)
 env.Append(CORE_FILES = core_files)
 
 env.Replace(
-    #CC="arm-none-eabi-g++",
-    #OBJCOPY="arm-none-eabi-objcopy"
-    CC="/home/m/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-g++",
-    OBJCOPY="/home/m/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-objcopy"
+    CC="arm-none-eabi-g++",
+    CXX="arm-none-eabi-g++",
+    LINK="arm-none-eabi-ld",
+    OBJCOPY="arm-none-eabi-objcopy"
 )
 
 print(env['CORE'])
@@ -65,7 +66,7 @@ print(env['CORE'])
 env.Append(
     BUILDERS = dict(
         build_elf_tmp=Builder(
-            action="$CC $CCFLAGS $CORE_FILES $SOURCE -I$LINKER_DIR -I$CORE -L$INCLUDE -T$GENERIC_GCC_NRF52_LD -o $TARGET",
+            action="$CC $CCFLAGS $CORE_FILES $SOURCE -I$LINKER_DIR -I$CORE -L$LINKER_DIR -T$GENERIC_GCC_NRF52_LD -o $TARGET",
         ),
         build_bin_tmp=Builder(
             action="$OBJCOPY -O binary $SOURCE $TARGET",
@@ -74,7 +75,7 @@ env.Append(
             action="$MICROAPP_MAKE_SCRIPT -i $SOURCE $HEADER_LD",
         ),
         build_elf=Builder(
-            action="$CC $CCFLAGS $CORE_FILES $SOURCE $HEADER_LD -I$CORE -I$LINKER_DIR -L$INCLUDE -T$GENERIC_GCC_NRF52_LD -o $TARGET",
+            action="$CC $CCFLAGS $CORE_FILES $SOURCE $HEADER_LD -I$CORE -I$LINKER_DIR -L$LINKER_DIR -T$GENERIC_GCC_NRF52_LD -o $TARGET",
         ),
         build_microapp_symbols=Builder(
             action="$CC -CC -E -P -x c -I$LINKER_DIR $SOURCE -o $TARGET",
@@ -96,19 +97,19 @@ if env.GetOption('clean'):
     create_empty_header_symbols_file(None, None, env)
 
 microapp_symbols = env.build_microapp_symbols(source="$SYMBOLS_IN", target="$SYMBOLS_LD")
-#env.AddPreAction(microapp_symbols, create_empty_header_symbols_file)
+env.AddPreAction(microapp_symbols, create_empty_header_symbols_file)
 
 firmware_elf_tmp = env.build_elf_tmp(source="$MAIN_C", target="$MAIN_ELF_TMP")
 Depends(firmware_elf_tmp, microapp_symbols)
 
 firmware_bin_tmp = env.build_bin_tmp(source="$MAIN_ELF_TMP", target="$MAIN_BIN_TMP")
 
-#firmware_header_ld = env.build_microapp_header_symbols(target="$HEADER_LD",source="$MAIN_BIN_TMP")
-#env.NoClean("$HEADER_LD")
-#env.AlwaysBuild("$HEADER_LD")
+firmware_header_ld = env.build_microapp_header_symbols(target="$HEADER_LD",source="$MAIN_BIN_TMP")
+env.NoClean("$HEADER_LD")
+env.AlwaysBuild("$HEADER_LD")
 
-#firmware_elf = env.build_elf(target='$MAIN_ELF', source='$MAIN_C')
-#Depends(firmware_elf, firmware_header_ld)
+firmware_elf = env.build_elf(target='$MAIN_ELF', source='$MAIN_C')
+Depends(firmware_elf, firmware_header_ld)
 
-#firmware_hex = env.build_hex(target="$MAIN_HEX", source="$MAIN_ELF")
-#firmware_bin = env.build_bin(target="$MAIN_BIN", source="$MAIN_ELF")
+firmware_hex = env.build_hex(target="$MAIN_HEX", source="$MAIN_ELF")
+firmware_bin = env.build_bin(target="$MAIN_BIN", source="$MAIN_ELF")
---
+++ 13.fix_ram_data_stripped_scons_env/scons/src/example.c
@@ -0,0 +1,104 @@
+#include <Arduino.h>
+//
+// An example to show a few functions being implemented.
+//
+
+// Show how a counter is incremented
+static int counter = 100;
+
+uint8_t serviceDataBuf[12] = {0};
+
+volatile byte state = LOW;
+
+void blink() {
+	state = !state;
+}
+
+const uint8_t BUTTON1_INDEX=4;
+
+//
+// A dummy setup function.
+//
+void setup() {
+	// We can "start" serial.
+	Serial.begin();
+
+	// We can use if(Serial), although this will always return true now (might be different in release mode).
+	if (!Serial) return;
+
+	// Write something to the log (will be shown in the bluenet code as print statement).
+	Serial.println("Setup started");
+
+	// We can also write integers.
+	Serial.println(counter);
+
+	// Set the UUID of this microapp.
+	serviceDataBuf[0] = 12;
+	serviceDataBuf[1] = 34;
+
+	// Set digital port 1 to OUTPUT, so we can write.
+	pinMode(1, OUTPUT);
+
+	// Join the i2c bus
+	Wire.begin();
+
+	// Set interrupt handler
+	pinMode(BUTTON1_INDEX, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(BUTTON1_INDEX), blink, CHANGE);
+}
+
+//
+// A dummy loop function.
+//
+int loop() {
+	// We are able to use static variables.
+	counter++;
+
+	// We can use local variables, also before and after delay() calls.
+	// int test = 1;
+	byte address = 0x18;
+
+	if (counter % 5 == 0) {
+		// Start transmission over i2c bus
+		Wire.beginTransmission(address);
+		Wire.write(5);
+		Wire.endTransmission();
+
+		Serial.print("Read req: ");
+
+		// Request a few bytes from device at given address
+		Wire.requestFrom(address, 2);
+
+		while (Wire.available()) {
+			char c = Wire.read();
+			Serial.write(c);
+		}
+		Serial.println(".");
+
+		if (state == LOW) {
+			Serial.println("State up");
+		} else {
+			Serial.println("State down");
+		}
+	}
+
+	if (counter % 10 == 0) {
+		Serial.println("Delay 10 sec");
+
+		// We delay 10 seconds.
+		delay(10000);
+
+		// See protocol definition for other options.
+		digitalWrite(1, 0);
+		
+		Serial.println("Done...");
+	}
+	// Show counter.
+	Serial.println(counter);
+
+	// Let's also advertise the latest counter value in the service data.
+	serviceDataBuf[2] = counter;
+	SerialServiceData.write(serviceDataBuf, sizeof(serviceDataBuf));
+
+	return counter;
+}
